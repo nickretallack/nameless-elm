b17e888cf5c0f7655051909ebb971b1b7db99d7c
Nick Retallack <nickretallack@gmail.com>
2018-11-25 14:11:41 -0800

    Use fragment identifiers for navigation

--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,12 @@ import Browser
 import Browser.Navigation as Nav
 import Url exposing (Url)
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 import Dict exposing (Dict)
 import Random exposing (Seed, initialSeed, step)
 import UUID exposing (UUID)
+import Debug exposing (log)
 
 -- main
 
@@ -28,9 +30,19 @@ type alias TranslatableString = Dict Language String
 type alias Language = String
 
 currentLanguage = "en"
+
 makeTranslatable : String -> TranslatableString
-makeTranslatable string = Dict.fromList [(currentLanguage, string)]
-getTranslatable dict = Dict.get currentLanguage
+makeTranslatable string =
+  Dict.fromList [(currentLanguage, string)]
+
+getTranslatable : TranslatableString -> String
+getTranslatable dict =
+  Maybe.withDefault "" (Dict.get currentLanguage dict)
+
+updateTranslatable : TranslatableString -> String -> TranslatableString
+updateTranslatable dict string =
+  Dict.insert currentLanguage string dict 
+
 
 --- implementation
 
@@ -89,21 +101,43 @@ type Msg
   = ChangedUrl Url
   | ClickedLink Browser.UrlRequest
   | NewConstant
+  | ChangeName DefinitionID String
 
 initialSeed = Random.initialSeed 12345
+
+isUuid maybeString =
+  case maybeString of
+    Nothing -> False
+    Just string ->
+      case UUID.fromString string of
+        Ok _ -> True
+        Err _ -> False
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
   case msg of
     ChangedUrl url ->
-      ({model | currentDefinitionID = Just url.path}, Cmd.none)
+      ({model | currentDefinitionID =
+        if (isUuid url.fragment) then Just url.path else Nothing}, Cmd.none)
     ClickedLink request ->
       (model, Cmd.none)
     NewConstant ->
       let (uuid, newSeed) = makeUuid initialSeed in
       ( editNames model (\names ->
           Dict.insert uuid (makeTranslatable "") names)
-      , Nav.pushUrl model.navKey uuid)
+      , Nav.pushUrl model.navKey ("#" ++ uuid))
+    ChangeName uuid newName ->
+      ( editNames model (\names -> 
+          Dict.update uuid (\maybeName ->
+            case maybeName of
+              Nothing ->
+                Just (makeTranslatable newName)
+              Just name ->
+                Just (updateTranslatable name newName)
+          ) names
+        )
+      , Cmd.none
+      )
 
 -- addConstant : Model -> UUID -> Model
 -- addConstant model uuid =
@@ -123,6 +157,13 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
+getName model definitionID =
+  case Dict.get definitionID model.names of
+    Nothing ->
+      ""
+    Just translatableString ->
+      getTranslatable translatableString
+
 --view
 
 view : Model -> Browser.Document Msg
@@ -136,7 +177,13 @@ view model =
             div [] [text "Not editing a constant"]
           
           Just definitionID ->
-            div [] [text "Editing a constant"]
+            div []
+            [ text ("Editing a constant: " ++ getName model definitionID)
+            , input
+              [ onInput (\text -> ChangeName definitionID text)
+              , value (getName model definitionID)
+              ] []
+            ]
       ]
     ]
   }
